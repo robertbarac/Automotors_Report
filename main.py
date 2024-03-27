f6f8552c1645a3683731a38cdb9131a53f5ef1ea
Robert Barrios <ryba-07@hotmail.com>
2024-03-26 21:50:57 -0500

    corregir el main

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import altair as alt
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# import altair as alt
 import plotly.express as px
 
 
